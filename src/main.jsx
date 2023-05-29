277f83cf470772681f561fd2a2c3009fc18bb4a5
Anton Vynnyk <anton.vynnyk.8118@gmail.com>
2023-05-29 09:10:57 +0300

    update-path

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter, HashRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store.js';
 import {homepage} from "./store/services/routes.js";
@@ -10,10 +10,10 @@ import {homepage} from "./store/services/routes.js";
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		{/*<BrowserRouter>*/}
-		<BrowserRouter basename={homepage}>
+		<HashRouter>
 			<Provider store={store}>
 				<App />
 			</Provider>
-		</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>
 );
